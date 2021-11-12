4e02e2960e3ab44d8728fc95e9b20291b0972ce0
Henrique Leite <henriqueleite616@gmail.com>
2021-11-12 17:51:24 -0300

    Add close Method Call (#33)

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# Todo
+
+- [ ] Check why `symbiosis.config.js` is not being loaded
--- src/lib/symbiosis-core.module.ts
+++ src/lib/symbiosis-core.module.ts
@@ -1,17 +1,50 @@
-import { DynamicModule, Global, Module } from "@nestjs/common";
-import { BaseConnectionOptions } from "@techmmunity/symbiosis";
+import {
+	DynamicModule,
+	Global,
+	Inject,
+	Module,
+	OnApplicationShutdown,
+	Provider,
+} from "@nestjs/common";
+import { ModuleRef } from "@nestjs/core";
+import {
+	BaseConnection,
+	BaseConnectionOptions,
+	Logger,
+} from "@techmmunity/symbiosis";
+import { SYMBIOSIS_MODULE_OPTIONS_ARRAY } from "./symbiosis.constants";
 import { SymbiosisPluginClass } from "./types/symbiosis";
 import { getConnectionToken } from "./utils/get-connection-token";
 
 @Global()
 @Module({})
-export class SymbiosisCoreModule {
+export class SymbiosisCoreModule implements OnApplicationShutdown {
+	public constructor(
+		@Inject(SYMBIOSIS_MODULE_OPTIONS_ARRAY)
+		private readonly options: Array<BaseConnectionOptions>,
+		private readonly moduleRef: ModuleRef,
+	) {}
+
 	public static forRoot<ConnectionOptions extends BaseConnectionOptions>(
 		// eslint-disable-next-line @typescript-eslint/naming-convention
 		ConnectionClass: any,
 		options?: ConnectionOptions,
 	): DynamicModule {
-		const connectionProvider = {
+		/**
+		 * Must defined a array of options, because have to
+		 * get the name of the connections to close it
+		 * (Symb supports multiples connections)
+		 */
+		const symbiosisModuleOptions: Provider = {
+			provide: SYMBIOSIS_MODULE_OPTIONS_ARRAY,
+			useFactory: (optionsProvider?: Array<BaseConnectionOptions>) => [
+				...(optionsProvider || []),
+				options,
+			],
+			inject: [SYMBIOSIS_MODULE_OPTIONS_ARRAY],
+		};
+
+		const connectionProvider: Provider = {
 			provide: getConnectionToken(options?.name),
 			useFactory: async () => {
 				const connection = new (ConnectionClass as SymbiosisPluginClass)(
@@ -27,8 +60,20 @@ export class SymbiosisCoreModule {
 
 		return {
 			module: SymbiosisCoreModule,
-			providers: [connectionProvider],
+			providers: [connectionProvider, symbiosisModuleOptions],
 			exports: [connectionProvider],
 		};
+	}
+
+	public async onApplicationShutdown() {
+		const connections = this.options.map(conOpt =>
+			this.moduleRef.get<BaseConnection>(getConnectionToken(conOpt.name)),
+		);
+
+		try {
+			await Promise.all(connections.map(con => con?.close()));
+		} catch (e: any) {
+			Logger.cliError(e?.message);
+		}
 	}
 }
